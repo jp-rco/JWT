5df744d96db867d2412af07bdeeb7b7578e7462a
Juan Pablo Restrepo <juanresco@unisabana.edu.co>
2024-05-10 19:08:53 -0500

    form fixed

--- my-react-app/src/pages/form/form.jsx
+++ my-react-app/src/pages/form/form.jsx
@@ -3,20 +3,23 @@ import Menu from '../../components/Menu';
 
 const Form = () => {
   const [text, setText] = useState('');
+  const [submittedText, setSubmittedText] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Simulate a POST request
-    fetch('http://localhost:5173/form', {
+    
+    // Realiza la solicitud HTTP POST al endpoint de tu API
+    fetch('https://jwt-gray.vercel.app/form', {
       method: 'POST',
+      body: JSON.stringify({ text }), // Envía el texto en el cuerpo de la solicitud
       headers: {
-        'Content-Type': 'text/plain'
-      },
-      body: text
+        'Content-Type': 'application/json'
+      }
     })
-      .then(response => response.text())
+      .then(response => response.json())
       .then(data => {
         console.log('Success:', data);
+        setSubmittedText(data.text); // Establece el texto devuelto en el estado
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -30,9 +33,9 @@ const Form = () => {
         <label htmlFor="text">Text:</label>
         <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} />
         <button type="submit">Submit</button>
-        <Menu page={"form"}/>
-
       </form>
+      {submittedText && <p>{submittedText}</p>} {/* Muestra el texto devuelto solo si hay uno */}
+      <Menu page={"form"}/>
     </div>
   );
 };
